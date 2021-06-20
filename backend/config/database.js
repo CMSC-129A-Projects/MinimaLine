bd7408bafec642ac77c7b5af81e90e3db50fd70f
Mira Mohammad <mohammira@gmail.com>
2021-06-20 18:00:55 +0800

    db config for heroku deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,12 +5,11 @@ const app = express();
 app.use(express.json());
 
 //connect to database
-const con = mysql.createConnection({
-    //port: 3005,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'MinimaLine'
+const con = mysql.createPool({
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bb894ec6e6df31',
+    password: 'c7693661',
+    database: 'heroku_265d1c24262a4ec'
 });
 
 module.exports = con;
