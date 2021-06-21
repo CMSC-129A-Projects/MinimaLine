c5280179339007fe06a7b6012e0c8869a1f39686
Mira Mohammad <mohammira@gmail.com>
2021-06-22 02:50:26 +0800

    changed db name in sql file

--- backend/MinimaLine.sql
+++ backend/MinimaLine.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `minimaline`
+-- Database: `heroku_265d1c24262a4ec`
 --
 
 -- --------------------------------------------------------
